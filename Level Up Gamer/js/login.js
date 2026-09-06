aaa1f11dd0e572beaf8986931d59512536f3c142
MatiasKorner <mat.korner@duocuc.cl>
2026-09-06 18:12:30 -0300

    Mejora login y agrega acceso administrativo

--- Level Up Gamer/js/login.js
+++ Level Up Gamer/js/login.js
@@ -5,8 +5,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const errorCorreo = document.getElementById("error-correo");
   const contraseñaInput = document.getElementById("contraseña");
   const errorContraseña = document.getElementById("error-contraseña");
+  const loginContenedor = document.querySelector(".login-contenedor");
+  const botonAdmin = document.getElementById("btn-admin");
+  const botonCliente = document.getElementById("btn-cliente");
+  const formularioAdmin = document.getElementById("form-admin");
+  const correoAdminInput = document.getElementById("correo-admin");
+  const errorCorreoAdmin = document.getElementById("error-correo-admin");
+  const contraseñaAdminInput = document.getElementById("contraseña-admin");
+  const errorContraseñaAdmin = document.getElementById("error-contraseña-admin");
   //obtenemos elementos del DOM.
 
+  const usuarioAdmin = [
+    {
+      correo: "admin@levelup.cl",
+      contraseña: "admin123",
+      rol: "Administrador"
+    },
+    {
+      correo: "vendedor@levelup.cl",
+      contraseña: "vende123",
+      rol: "Vendedor"
+    }
+  ];
+
+  botonAdmin.addEventListener("click", () => {
+    loginContenedor.classList.add("admin-activo");
+  });
+
+  botonCliente.addEventListener("click", () => {
+    loginContenedor.classList.remove("admin-activo");
+  });
   formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -40,6 +68,57 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "index.html";
   });
 
+  formularioAdmin.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const correoAdmin = correoAdminInput.value.trim();
+
+  if (!correoAdmin.endsWith("@levelup.cl")) {
+    errorCorreoAdmin.textContent =
+      "Ingresa un correo administrativo @levelup.cl";
+
+    correoAdminInput.classList.add("is-invalid");
+    return;
+  }
+
+  errorCorreoAdmin.textContent = "";
+  correoAdminInput.classList.remove("is-invalid");
+
+  const contraseñaAdmin = contraseñaAdminInput.value.trim();
+
+  if (!validarContraseña(contraseñaAdmin)) {
+    errorContraseñaAdmin.textContent =
+      "La contraseña debe tener entre 4 y 10 caracteres";
+
+    contraseñaAdminInput.classList.add("is-invalid");
+    return;
+  }
+
+  errorContraseñaAdmin.textContent = "";
+  contraseñaAdminInput.classList.remove("is-invalid");
+  
+  const usuarioEncontrado = usuarioAdmin.find(usuario =>
+    usuario.correo === correoAdmin &&
+    usuario.contraseña === contraseñaAdmin
+  );
+
+  if (!usuarioEncontrado){
+    errorContraseñaAdmin.textContent = "Correo o contraseña incorrectos"
+    contraseñaAdminInput.classList.add("is-invalid");
+    return;
+  }
+
+  errorContraseñaAdmin.textContent = "";
+  contraseñaAdminInput.classList.remove("is-invalid");
+
+  localStorage.setItem("lug_correo", usuarioEncontrado.correo);
+  localStorage.setItem("lug_rol", usuarioEncontrado.rol);
+
+  alert("Bienvenido " + usuarioEncontrado.rol);
+
+  
+  // Más adelante aquí validaremos el rol y el acceso administrativo.
+});
   correoInput.addEventListener("input", () => {
     const correo = correoInput.value.trim();
 
@@ -69,9 +148,37 @@ document.addEventListener("DOMContentLoaded", () => {
       errorContraseña.textContent = "";
       contraseñaInput.classList.remove("is-invalid");
     }
+  });
+  correoAdminInput.addEventListener("input", () => {
+  const correoAdmin = correoAdminInput.value.trim();
+
+  if (correoAdmin.includes("@") && !correoAdmin.endsWith("@levelup.cl")) {
+    errorCorreoAdmin.textContent =
+      "Ingresa un correo administrativo @levelup.cl";
+
+    correoAdminInput.classList.add("is-invalid");
+  } else {
+    errorCorreoAdmin.textContent = "";
+    correoAdminInput.classList.remove("is-invalid");
+  }
+});
+
+contraseñaAdminInput.addEventListener("input", () => {
+  const contraseñaAdmin = contraseñaAdminInput.value;
+
+  if (contraseñaAdmin !== "" && !validarContraseña(contraseñaAdmin)) {
+    errorContraseñaAdmin.textContent =
+      "La contraseña debe tener entre 4 y 10 caracteres";
+
+    contraseñaAdminInput.classList.add("is-invalid");
+  } else {
+    errorContraseñaAdmin.textContent = "";
+    contraseñaAdminInput.classList.remove("is-invalid");
+  }
 });
 
 });
+
 
 // Validación de correo electrónico
 function validarCorreo(correo) {
